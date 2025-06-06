41e089bfed453989021cc6834004aa6276c0bd3f
Raj Amancha <therajamancha@gmail.com>
2025-06-06 13:07:14 +0530

    create video section and use in the about page

---
+++ src/components/sections/video-section.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoSection = () => {
+  return (
+    <section>
+      <video src="/videos/video.mp4" autoPlay muted loop />
+    </section>
+  )
+}
+
+export default VideoSection
